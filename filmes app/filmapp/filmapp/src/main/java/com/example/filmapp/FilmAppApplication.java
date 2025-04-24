c6618008d79bb07e27339aa29f4d5b8f1efd0223
Patrik9777 <peterpatrik9777@gmail.com>
2025-04-24 12:41:19 +0200

    Film lista

--- filmes app/filmapp/filmapp/src/main/java/com/example/filmapp/FilmAppApplication.java
+++ filmes app/filmapp/filmapp/src/main/java/com/example/filmapp/FilmAppApplication.java
@@ -1,12 +1,30 @@
+/**
+ * This package contains the main application files for FilmApp.
+ */
 package com.example.filmapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Main application class for FilmApp.
+ */
 @SpringBootApplication
 public class FilmAppApplication {
-    public static void main(String[] args) {
+
+    /**
+     * Private constructor to prevent instantiation of this utility class.
+     */
+    private FilmAppApplication() {
+        throw new UnsupportedOperationException("Utility class");
+    }
+
+    /**
+     * A main method that serves as the entry point for the application.
+     *
+     * @param args the command-line arguments
+     */
+    public static void main(final String[] args) {
         SpringApplication.run(FilmAppApplication.class, args);
     }
 }
-// vége
